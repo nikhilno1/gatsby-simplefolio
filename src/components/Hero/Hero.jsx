1a31796ab0925c5fefde901e349b8d491e88d739
Nikhil <nikhil.utane@gmail.com>
2023-06-13 09:54:14 +0530

    Update Hero.jsx
    Changed default text

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -26,12 +26,12 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
+            {title || 'Portfolio - Nikhil Utane'}{' '}
             <span className="text-color-main">{name || 'Your Name'}</span>
             <br />            
           </h1>
           <h2 className="hero-subtitle">
-            {subtitle || "I'm the Unknown Developer."}
+            {subtitle || "I'm a seasoned Technology Leader"}
           </h2>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
